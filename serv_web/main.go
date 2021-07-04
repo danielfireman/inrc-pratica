ecb63d118dba2c79a08a670e40e9fa65897828fb
Daniel Fireman <daniel.fireman@ifal.edu.br>
2021-07-04 11:44:52 -0300

    mais código

--- serv_web/main.go
+++ serv_web/main.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, req *http.Request) {
-	pagina := `
-	<html>
-	<h1>Olá Mundo</h1>
-	</html>
+	// Topo da página
+	topo := `
+<html>
+<head>
+	<title>Bem-vindas ao Frases Aleatórias Tabajara</title>
+</head>
+<h1>Bem-vindas ao Frases Aleatórias Tabajara</h1>
+<img
+	src="https://uploads.spiritfanfiction.com/fanfics/historias/201809/frases-aleatorias-14335368-190920182223.jpg"
+	alt="Livro com capa vermelha escrita Frases Aleatórias"
+	height=150
+	width=150
+/>
 `
-	fmt.Fprint(w, pagina)
+	// Realizando requisição e interpretando resultados.
+	http.Get("https://allugofrases.herokuapp.com/frases/random")
+
+	// Final da página
+	fim := `
+</html>
+`
+	fmt.Fprintf(w, "%s%s", topo, fim)
+}
+
+func reqFraseAleatória() (string, error) {
+	resp, err := http.Get("https://allugofrases.herokuapp.com/frases/random")
+	if err != nil {
+		return "", fmt.Errorf("erro realizando http get:%v", err)
+	}
+	defer resp.Body.Close()
+
+	dados, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("erro fazendo interpretando resposta:%v", err)
+	}
 }
 
 func main() {
