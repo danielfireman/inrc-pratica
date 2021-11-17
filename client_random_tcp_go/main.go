19691444b7c0a4874c5581c188d06cb1dfc1b7b3
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-11-17 00:26:02 +0000

    melhorando cliente TCP

--- client_random_tcp_go/main.go
+++ client_random_tcp_go/main.go
@@ -15,20 +15,32 @@ func main() {
 	}
 	fmt.Println("Conectado a", os.Args[1])
 
-	// Enviando mensagem seguindo o protocolo do servidor (o \n é importante)
-	msg := fmt.Sprintf("%s\n", os.Args[2])
-	if _, err := conn.Write([]byte(msg)); err != nil {
-		log.Fatalf("Erro escrevendo na conexão TCP %s:%v", os.Args[1], err)
-	}
-	fmt.Println("Mensagem:", msg, "escritos em", os.Args[1])
+	for {
+		fmt.Printf("Digite a mensagem a ser enviada para o servidor:")
+		var msg string
+		if _, err := fmt.Scanln(&msg); err != nil {
+			log.Fatalf("Erro lendo do teclado:%v", err)
+		}
 
-	// Lendo resposta do servidor.
-	buf := make([]byte, 1024)
-	if _, err := conn.Read(buf); err != nil {
-		log.Fatalf("Erro lendo da conexão TCP %s:%v", os.Args[1], err)
+		if msg == "encerra" {
+			break
+		}
+
+		// Enviando mensagem seguindo o protocolo do servidor (o \n é importante)
+		if _, err := conn.Write([]byte(fmt.Sprintf("%s\n", msg))); err != nil {
+			log.Fatalf("Erro escrevendo na conexão TCP %s:%v", os.Args[1], err)
+		}
+		fmt.Println("Mensagem:", msg, "escritos em", os.Args[1])
+
+		// Lendo resposta do servidor.
+		buf := make([]byte, 1024)
+		if _, err := conn.Read(buf); err != nil {
+			log.Fatalf("Erro lendo da conexão TCP %s:%v", os.Args[1], err)
+		}
+		fmt.Println("Resposta:", string(buf), "lida de", os.Args[1])
 	}
-	fmt.Println("Resposta:", string(buf), "lida de", os.Args[1])
 
 	// Fechando conexão.
 	conn.Close()
+	fmt.Printf("Conexão com %s encerrada.\n", os.Args[1])
 }
